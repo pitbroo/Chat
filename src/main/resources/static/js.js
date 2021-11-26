0cffcf07bc19f74f80deec8a4a4bd11a1d7f6ab5
Piotr <pitbroo@gmail.com>
2021-11-26 13:52:56 +0100

    add pretty design

--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -3,9 +3,22 @@ var client = null;
 function showMessage(newMessage) {
     var response = document.getElementById('response');
     var newResponse = document.createElement('p');
+    var date = document.createElement('h5');
+
+    var now= new Date();
+    now = now.getDate()+"-"+ now.getMonth()+"-"+now.getFullYear()+", "+ now.getHours()+":"
+        + now.getMinutes()+":"+now.getSeconds()+":";
+
+
+     date.appendChild(document.createTextNode(now));
+     date.classList.add('date');
+     response.appendChild(date);
+
     newResponse.classList.add('list-group-item')
     newResponse.appendChild(document.createTextNode(newMessage));
-    response.appendChild(newResponse)
+    response.appendChild(newResponse);
+    //go to down of site
+    window.scrollTo(0,document.body.scrollHeight);
 
 }
 function  connect(){
@@ -21,6 +34,7 @@ function sendMessage() {
     var messageToSend = document.getElementById('messageToSend').value;
     client.send("/app/chat", {}, JSON.stringify({'value': messageToSend}));
     clearMtS();
+
 
 }
 function clearMtS(){
