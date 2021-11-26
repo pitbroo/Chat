74db14081a5cb01bfd4d4ce7d29bb3b1f4ab3957
Piotr <pitbroo@gmail.com>
2021-11-26 15:49:27 +0100

    add User

--- src/main/java/com/example/chat/ChatMessage.java
+++ src/main/java/com/example/chat/ChatMessage.java
@@ -5,9 +5,11 @@ import lombok.Data;
 @Data
 public class ChatMessage {
     private String value;
+    private String user;
 
-    public ChatMessage(String value) {
+    public ChatMessage(String value, String user) {
         this.value = value;
+        this.user = user;
     }
 
     public ChatMessage() {
--- src/main/resources/static/js.js
+++ src/main/resources/static/js.js
@@ -1,22 +1,28 @@
 var client = null;
 
-function showMessage(newMessage) {
+function showMessage(newMessage, user) {
     var response = document.getElementById('response');
-    var newResponse = document.createElement('p');
-    var date = document.createElement('h5');
+    var newResponse = document.createElement('h5');
+    var date = document.createElement('p');
+    var userElement = document.createElement('p');
 
+    //set date
     var now= new Date();
     now = now.getDate()+"-"+ now.getMonth()+"-"+now.getFullYear()+", "+ now.getHours()+":"
         + now.getMinutes()+":"+now.getSeconds()+":";
-
-
      date.appendChild(document.createTextNode(now));
      date.classList.add('date');
-     response.appendChild(date);
+
+     //set user
+    userElement.classList.add('user');
+    userElement.appendChild(document.createTextNode(user))
 
     newResponse.classList.add('list-group-item')
     newResponse.appendChild(document.createTextNode(newMessage));
-    response.appendChild(newResponse);
+
+    response.appendChild(userElement);
+    userElement.appendChild(date)
+    userElement.appendChild(newResponse);
     //go to down of site
     window.scrollTo(0,document.body.scrollHeight);
 
@@ -25,14 +31,15 @@ function  connect(){
     client = Stomp.client('ws://localhost:8080/chat');
     client.connect({}, function (frame){
      client.subscribe("/topic/messages", function frame(message){
-         showMessage(JSON.parse(message.body).value)
+         showMessage(JSON.parse(message.body).value, JSON.parse(message.body).user)
      })
  })
 }
 
 function sendMessage() {
     var messageToSend = document.getElementById('messageToSend').value;
-    client.send("/app/chat", {}, JSON.stringify({'value': messageToSend}));
+    var user = document.getElementById('user').value;
+    client.send("/app/chat", {}, JSON.stringify({'value': messageToSend, 'user': user} ));
     clearMtS();
 
 
