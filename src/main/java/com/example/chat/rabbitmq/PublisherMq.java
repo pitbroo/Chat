ba46223eac2e23c37dfc5780b0485c522fade78a
Rafał Balcerzak <rafalbalcerzak98@gmail.com>
2021-11-26 17:34:58 +0100

    - Add RabbitMQ configuration - add rabbitmq getRequest for send message

---
+++ src/main/java/com/example/chat/rabbitmq/PublisherMq.java
@@ -0,0 +1,20 @@
+package com.example.chat.rabbitmq;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PublisherMq {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @GetMapping("/addMessage")
+    public String get(@RequestParam String message) {
+        rabbitTemplate.convertAndSend("message", message);
+        return "sent" ;
+    }
+}
